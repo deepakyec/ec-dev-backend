157364aba382088a96ee0d199600ce52d16b9c5f
deepakyec <deepak.yadav@experiencecommerce.com>
2017-11-12 03:59:35 +0530

    Post , comments added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+
+	def about
+		@title ='About Us';
+		@content='This is About Us Page...';
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :widgets
+ #resources :widgets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index', as: 'home'
+  get 'about' => 'pages#about', as:'about'
+  resources :posts do
+  	resources :comments
+  end
+
 end
